2907d1fe74768454c046a5411ebb3440fe7aed03
Kim Andersson <kandersson@gmail.com>
2024-09-02 23:02:08 +0200

    a background component, with 3d effect

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import Background from './component/Background.tsx';
 
 createRoot(document.querySelector('#root')!).render(
   <StrictMode>
+    <Background />
     <App />
   </StrictMode>,
 );
