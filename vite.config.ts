72f725975f1ad7286064c758c233860247324a02
Kim Andersson <kandersson@gmail.com>
2024-09-05 11:57:38 +0200

    vite - test over lan - for mobile test some minor graphics change for animation

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    host: true, // eller specifik IP-adress om så önskas
+    port: 3000, // valfri port
+  }
 })
