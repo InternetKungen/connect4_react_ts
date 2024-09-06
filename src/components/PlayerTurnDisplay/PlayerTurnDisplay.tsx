1a7176b631a782c387fbb9552d6231dad561e9c5
Kim Andersson <kandersson@gmail.com>
2024-09-06 09:37:53 +0200

    init player turn display

---
+++ src/components/PlayerTurnDisplay/PlayerTurnDisplay.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const PlayerTurnDisplay: React.FC = (playerTurn) => {
+  return <div className="player-turn-display">
+
+    <div className="player-turn-symbol">
+    
+      <div className="left">
+      </div>
+    
+      <div className='right'>
+      </div>
+  
+    </div>
+
+  </div>  
+};
+
+export default PlayerTurnDisplay;
